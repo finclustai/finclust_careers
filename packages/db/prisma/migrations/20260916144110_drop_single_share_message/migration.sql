e19fe29924fd07aa61bbcbfbf8a03d943980e6e2
karthikeya-11 <karthikeyaananthula11@gmail.com>
2026-09-16 20:13:03 +0530

    Stop reading the old single share_message column
    Its data moved to share_messages in the previous migration. The drop
    migration is applied once this code is live, so production never reads
    a column that is gone.

---
+++ packages/db/prisma/migrations/20260916144110_drop_single_share_message/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `share_message` on the `job_openings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "job_openings" DROP COLUMN "share_message";
